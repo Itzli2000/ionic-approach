1ea1318bac233ce4716b756970ec208359291b81
Ariel  Molina <itzli2000@msn.com>
2021-02-15 12:45:07 -0600

    Feat: Completed page for buttons

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -24,6 +24,11 @@ export class HomePage implements OnInit {
       name: 'avatar',
       redirectTo: '/avatar'
     },
+    {
+      icon: 'radio-button-off-outline',
+      name: 'buttons',
+      redirectTo: '/buttons'
+    },
   ];
 
   constructor() { }
