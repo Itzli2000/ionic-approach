29873eb2e389056e628192007efa4ee58fb44130
Ariel  Molina <itzli2000@msn.com>
2021-02-22 14:49:40 -0600

    Feat: completed segment page
    Moddify filter pipe to handle search by 'column'
    Refactor search bar page header

--- src/app/pipes/filtro.pipe.ts
+++ src/app/pipes/filtro.pipe.ts
@@ -4,8 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filtro',
 })
 export class FiltroPipe implements PipeTransform {
-  transform(arreglo: any[], text: string = ''): any[] {
-    if (text === '' || text === undefined) {
+  transform(arreglo: any[], text: string = '', column: string = 'title'): any[] {
+    if (text === '' || text === undefined || text === 'all') {
       return arreglo;
     }
     if (!arreglo || arreglo.length <= 0) {
@@ -13,6 +13,6 @@ export class FiltroPipe implements PipeTransform {
     }
 
     text = text.toLowerCase();
-    return arreglo.filter((item) => item.title.toLowerCase().includes(text));
+    return arreglo.filter((item) => item[column].toLowerCase().includes(text));
   }
 }
